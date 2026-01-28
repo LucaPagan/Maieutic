e9537ad064954a592b448a867f885a24bf0743aa
Luca Pagano <149117354+LucaPagan@users.noreply.github.com>
2026-01-28 09:45:18 +0100

    Initial Commit

---
+++ TuttMockat/TuttMockatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuttMockatApp.swift
+//  TuttMockat
+//
+//  Created by Luca Pagano on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TuttMockatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
